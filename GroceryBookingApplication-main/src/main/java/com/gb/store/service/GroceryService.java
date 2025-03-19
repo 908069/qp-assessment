9030ef3d5eb9dab577a2134edd9753650f4136ea
Harsh agarwal <harsh.agarwal2018@gmail.com>
2025-03-19 07:52:28 +0530

    GorceryApp

---
+++ GroceryBookingApplication-main/src/main/java/com/gb/store/service/GroceryService.java
@@ -0,0 +1,143 @@
+package com.gb.store.service;
+
+import com.gb.store.exception.GroceryNotFoundException;
+import com.gb.store.exception.InsufficientInventoryException;
+import com.gb.store.model.request.GroceryItemRequest;
+import com.gb.store.model.request.GroceryItemUpdateRequest;
+import com.gb.store.model.response.GroceryItemResponse;
+import com.gb.store.repo.GroceryItemRepository;
+import com.gb.store.repo.entity.GroceryItem;
+import com.gb.store.utils.GroceryItemMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service to manager grocery items
+ */
+@Slf4j
+@Service
+public class GroceryService {
+
+    @Autowired
+    private GroceryItemRepository groceryItemRepository;
+    @Autowired
+    private GroceryItemMapper groceryItemMapper;
+
+
+    /**
+     * Fetch an existing grocery item by the id
+     * @param groceryItemId the id representing the grocery item in store
+     * @return GroceryItem represented by groceryItemId
+     */
+    public GroceryItem getGroceryItemById(String groceryItemId) {
+        Optional<GroceryItem> groceryItemOptional = groceryItemRepository.findById(groceryItemId);
+        if(groceryItemOptional.isEmpty()){
+            log.error("Invalid Grocery Item ID.");
+            throw new GroceryNotFoundException("Invalid Grocery Item ID.");
+        }
+        return groceryItemOptional.get();
+    }
+
+    /**
+     * Fetch all groceries form store
+     * @return all grocery items in store
+     */
+    @Transactional(readOnly = true)
+    public Page<GroceryItemResponse> getAllGroceryItems(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(pageNo,pageSize,sort);
+
+        Page<GroceryItem> groceriesPage = groceryItemRepository.findAll(pageable);
+
+        List<GroceryItemResponse> groceriesResponsePage
+                = groceriesPage.map(this.groceryItemMapper::mapToGroceryItemResponse).toList();
+
+        return new PageImpl<>(groceriesResponsePage,pageable,groceriesPage.getTotalElements());
+
+    }
+
+    /**
+     * Add new grocery item in store
+     * @param groceryItemRequest represent new grocery item to be added in store
+     */
+    @Transactional
+    public void addGrocery(GroceryItemRequest groceryItemRequest) {
+        log.info("Grocery Item Request : {}",groceryItemRequest);
+        GroceryItem groceryItem = groceryItemMapper.mapToGroceryItem(groceryItemRequest);
+        groceryItemRepository.save(groceryItem);
+    }
+
+    /**
+     * Update an existing grocery item in store
+     * @param updateRequest represent existing grocery item update changes
+     * @return update grocery item details
+     */
+    @Transactional
+    public GroceryItemResponse updateGrocery(GroceryItemUpdateRequest updateRequest) {
+        log.info("update request : {}",updateRequest);
+
+        String groceryItemId = updateRequest.getId();
+        Optional<GroceryItem> groceryItemOptional = groceryItemRepository.findById(groceryItemId);
+
+        if(groceryItemOptional.isEmpty()){
+            log.error("Invalid Grocery Item ID.");
+            throw new GroceryNotFoundException("Invalid Grocery Item ID.");
+        }
+
+        GroceryItem groceryItem = groceryItemOptional.get();
+
+        if(updateRequest.getName() != null)
+            groceryItem.setName(updateRequest.getName());
+
+        if(updateRequest.getPrice() != null)
+            groceryItem.setPrice(updateRequest.getPrice());
+
+        if(updateRequest.getQuantity() != null)
+            groceryItem.setQuantity(updateRequest.getQuantity());
+
+        GroceryItem groceryItemUpdated =  groceryItemRepository.save(groceryItem);
+
+        return this.groceryItemMapper.mapToGroceryItemResponse(groceryItemUpdated);
+    }
+
+    /**
+     * Delete an existing grocery item in store
+     * @param groceryItemId the id representing the grocery item in store
+     */
+    @Transactional
+    public void deleteGrocery(String groceryItemId) {
+        log.error("Deleting Grocery Item Id : {}",groceryItemId);
+        Optional<GroceryItem> groceryItemOptional = groceryItemRepository.findById(groceryItemId);
+        if(groceryItemOptional.isEmpty()){
+            log.error("Invalid Grocery Item ID.");
+            throw new GroceryNotFoundException("Invalid Grocery Item ID.");
+        }
+        groceryItemRepository.deleteById(groceryItemId);
+    }
+
+    /**
+     * The order quantity by the quantity for which the order is placed
+     * @param groceryItemId the id representing the grocery item in store
+     * @param quantity quantity to deduct
+     * @return Updated GroceryItem after reducing quantity
+     */
+    public GroceryItem manageInventory(String groceryItemId, int quantity) {
+
+        GroceryItem groceryItem = getGroceryItemById(groceryItemId);
+        Integer groceryItemQuantity = groceryItem.getQuantity();
+
+        if(quantity > groceryItemQuantity){
+            log.error("Order quantity is more than available quantity.");
+            throw new InsufficientInventoryException("Ordered quantity is more than available quantity.");
+        }
+
+        groceryItem.setQuantity(groceryItemQuantity - quantity);
+        return groceryItemRepository.save(groceryItem);
+    }
+}
---
+++ GroceryBookingApplication-main/src/main/java/com/gb/store/service/OrderService.java
@@ -0,0 +1,60 @@
+package com.gb.store.service;
+
+import com.gb.store.model.request.GroceryOrderRequest;
+import com.gb.store.repo.OrderRepository;
+import com.gb.store.repo.entity.GroceryItem;
+import com.gb.store.repo.entity.OrderInfo;
+import com.gb.store.repo.entity.OrderItem;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Service to manage order details, transaction and manage inventory after order
+ */
+@Slf4j
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private GroceryService groceryService;
+
+    /**
+     * Place an order by reducing the quanity of items from provided item id
+     * and quantity to reduce
+     * @param groceryOrderRequest grocery order request list
+     */
+    @Transactional
+    public void placeOrder(List<GroceryOrderRequest> groceryOrderRequest) {
+        Map<String, GroceryItem> processedGroceryItems = groceryOrderRequest.stream()
+                .map(orderItem -> {
+                    log.info("Ordered Item : {}", orderItem);
+                    // Adjust inventory after successful booking (decrease quantities)
+                    return groceryService.manageInventory(orderItem.getGroceryItemId(), orderItem.getQuantity());
+                })
+                .collect(Collectors.toMap(GroceryItem::getId, groceryItem -> groceryItem));
+
+        //store the ordered items transaction
+        if (!CollectionUtils.isEmpty(processedGroceryItems)){
+            OrderInfo orderInfo = new OrderInfo();
+            List<OrderItem> orderItemList = groceryOrderRequest.stream()
+                    .map(orderItem ->
+                            OrderItem.builder()
+                                    .groceryItem(processedGroceryItems.get(orderItem.getGroceryItemId()))
+                                    .quantity(orderItem.getQuantity())
+                                    .build()
+                    ).collect(Collectors.toList());
+
+            orderInfo.setOrderItems(orderItemList);
+            orderRepository.save(orderInfo);
+        }
+    }
+}
